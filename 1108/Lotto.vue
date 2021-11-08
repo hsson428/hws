7ffdbdb59f6b962a9bf27efccd090b805d2a6b83
hs <gudtjs0428@gmail.com>
2021-11-08 18:01:31 +0900

    1108

---
+++ 1108/Lotto.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Lotto</h1>
+    <button @click="getLuckyNumbers">Get Lucky Numbers</button>
+    <p v-if="pick.length">{{ pick }}</p>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+  name: 'Lotto',
+  data: function () {
+    return {
+      numbers: _.range(1, 46),
+      pick: [],
+    }
+  },
+  methods: {
+    getLuckyNumbers () {
+      this.pick = _.sortBy(_.sampleSize(this.numbers, 6))
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 1108/Lunch.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Lunch</h1>
+    <button @click="pickMenu">pick</button>
+    <p>{{ pick }}</p>
+    <h3>로또를 뽑아보자</h3>
+    <button @click="moveToLotto">Pick Lotto</button>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+  name: 'Lunch',
+  data: function () {
+    return {
+      menus: ['짜장면', '김치찌개', '고등어구이'],
+      pick: '',
+    }
+  },
+  methods: {
+    pickMenu () {
+      this.pick = _.sample(this.menus)
+    },
+    moveToLotto() {
+      this.$router.push({ name: 'Lotto'})
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
